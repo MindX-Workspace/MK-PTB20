88f7c826775b4bd1760b25015a3971e8b33e8059
whis102 <chicongnguyen.dev@gmail.com>
2024-07-25 23:07:01 +0700

    update

---
+++ B2/index.py
@@ -0,0 +1,4 @@
+print("Bảng cửu chương 2")
+
+print("2 x 1 = ", 2*1)
+print("2 x 9 = ", 2*9)
---
+++ B2/main.py
@@ -0,0 +1,4 @@
+usd = int(input("Nhập số tiền mà bạn cần chuyển (USD): "))
+vnd = usd * 25000
+
+print("Với " + str(usd) + " USD bạn đổi được " + str(vnd) + " VND")
