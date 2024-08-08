7cbfca78379a977eff209cce89204dd75a5f6b96
whis102 <chicongnguyen.dev@gmail.com>
2024-08-08 20:56:21 +0700

    update

---
+++ B4/main.py
@@ -0,0 +1,6 @@
+n = int(input("Nhập vào một số để kiểm tra tính chẵn lẻ: "))
+
+if n % 2 == 0:
+    print("n là số chẵn")
+else:
+    print("n là số lẻ")
---
+++ B4/score.py
@@ -0,0 +1,10 @@
+score = float(input("Nhập vào số điểm của bạn: "))
+
+if score >= 8:
+    print("Hoc sinh gioi")
+elif score >= 6.5:
+    print("Hoc sinh kha")
+elif score >= 5:
+    print("Hoc sinh trung binh")
+else:
+    print("Hoc sinh yeu")
